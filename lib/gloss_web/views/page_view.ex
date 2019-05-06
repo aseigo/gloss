26d315aa4425cd8aaf2253756064103f2e052f93
Aaron Seigo <aaron@nomoko.world>
2019-05-06 15:20:02 +0200

    links for jumplist, but do not use the jumplist just yet

--- lib/gloss_web/views/page_view.ex
+++ lib/gloss_web/views/page_view.ex
@@ -1,15 +1,31 @@
 defmodule GlossWeb.PageView do
   use GlossWeb, :view
+  import Ecto.Query, only: [from: 2]
 
-  def letter_jumps() do
-    Enum.reduce(90..65, [], fn x, acc -> [[x] | acc] end) |> Enum.join(" &middot; ")
-  end
+  def letter_jumps(current_section) do
+    query = from l in "words",
+      select: fragment("left(?, 1)", l.word),
+      where: l.section_id == ^current_section,
+      group_by: l.word,
+      order_by: fragment("left(?, 1)", l.word)
 
-  def current_section() do
-    
+    letters = query
+              |> Gloss.Repo.all()
+              |> Enum.reduce(MapSet.new(), &(MapSet.put(&2, &1)))
+
+    Enum.reduce(?Z..?A, [], fn x, acc -> [jump_text([x], letters) | acc] end)
+    |> Enum.join(" &middot; ")
   end
 
   def wordlist(category_id, prefix) do
     Gloss.Glossary.list_words(category_id, prefix)
+  end
+
+  defp jump_text(x, letters) do
+    if MapSet.member?(letters, to_string([x])) do
+      ~s(<a href="##{x}">#{x}</a>)
+    else
+      x
+    end
   end
 end
