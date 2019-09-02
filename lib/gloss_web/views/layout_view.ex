29f5184ee86649aff16662c78488082d077bc7c7
Aaron Seigo <aaron@nomoko.world>
2019-09-02 13:00:55 +0200

    don't crash when there are no sections yet

--- lib/gloss_web/views/layout_view.ex
+++ lib/gloss_web/views/layout_view.ex
@@ -4,5 +4,8 @@ defmodule GlossWeb.LayoutView do
   def sections() do
     Gloss.Glossary.list_sections()
     |> Enum.reduce([], fn section, acc -> [[value: section.id, key: section.name] | acc] end)
+    |> (fn [] -> [[key: "No sections", value: 0]]
+           acc -> acc
+        end).()
   end
 end
